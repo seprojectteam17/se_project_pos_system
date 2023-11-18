b7c0e0ab5475829c5c208a467a254ef4a640f6db
u g dheeraj sai <dheerajsaipro16@gmail.com>
2023-11-18 11:57:16 +0530

    created features to see items and add items to cart

--- employee.py
+++ employee.py
@@ -116,6 +116,50 @@ class login_page:
         self.button1.configure(borderwidth="0")
         self.button1.configure(text="""LOGIN""")
         self.button1.configure(command=login)
+class Item:
+    def __init__(self, name, price, qty):
+        self.product_name = name
+        self.price = price
+        self.qty = qty
+
+class Cart:
+    def __init__(self):
+        self.items = []
+        self.dictionary = {}
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def remove_item(self):
+        self.items.pop()
+
+    def remove_items(self):
+        self.items.clear()
+
+    def total(self):
+        total = 0.0
+        for i in self.items:
+            total += i.price * i.qty
+        return total
+
+    def isEmpty(self):
+        if len(self.items)==0:
+            return True
+        
+    def allCart(self):
+        for i in self.items:
+            if (i.product_name in self.dictionary):
+                self.dictionary[i.product_name] += i.qty
+            else:
+                self.dictionary.update({i.product_name:i.qty})
+    
+
+def exitt():
+    sure = messagebox.askyesno("Exit","Are you sure you want to exit?", parent=biller)
+    if sure == True:
+        biller.destroy()
+        root.destroy()
+        
 page1 = login_page(root)
 root.bind("<Return>", login)
 root.mainloop()
